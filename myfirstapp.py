f0f93851643db648d91ef8fe91ea225f85ce465a
ayyinz <154412159+ayyinz@users.noreply.github.com>
2023-12-20 16:47:27 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,23 +1,26 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import time
 
 st.header("My first Streamlit App")
 
 option = st.sidebar.selectbox(
     'Select a mini project',
-     ['line chart','map','T n C'])
+     ['line chart','map','T n C','Long Process'])
 
 if option=='line chart':
     chart_data = pd.DataFrame(
-      np.random.randn(20, 3),
-      columns=['a', 'b', 'c'])
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
     st.line_chart(chart_data)
 
 elif option=='map':
     map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [3.1242, 101.6861],
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
+
     st.map(map_data)
 
 elif option=='T n C':
@@ -39,3 +42,4 @@ elif option=='Long Process':
         latest_iteration.text(f'Iteration {i+1}')
         bar.progress(i + 1)
         time.sleep(0.1)
+       
